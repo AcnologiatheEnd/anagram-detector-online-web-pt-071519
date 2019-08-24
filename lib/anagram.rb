f85abe2838f38babed6810d80f27b7ed2b06fd79
Winston  Chung <18.chung.winston@ctkprep.org>
2019-08-24 23:12:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,7 @@ class Anagram
   
   def match(array)
     word_splitter = @word.split("").sort
+    new_array = []
     for each in array
       if each.split("").sort == word_splitter
         return each
